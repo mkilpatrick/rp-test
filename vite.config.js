f03c585fc46c7838fe1567bada0ae7125a4cabb9
Duval Kilpatrick <mkilpatrick@yext.com>
2023-06-05 12:04:54 -0400

    test renderBuiltUrl

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
 
 export default defineConfig({
-  base: "",
+  // base: "",
   plugins: [react(), yextSSG()],
+  experimental: {
+    renderBuiltUrl(filename, { hostId, hostType, type }) {
+      if (hostType === 'js') {
+        if (filename.at(0) === '/') {
+          return filename.substring(1);
+        }
+        return filename;
+      }
+      return { relative: true }
+
+      // return "hostId:" + hostId + "hostType:" + JSON.stringify(hostType) + "type:" + type + "filename:" + filename;
+    }
+  }
 });
