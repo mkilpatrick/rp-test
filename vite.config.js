839c7892eea72c453a34e6b3468bd25052884426
Duval Kilpatrick <mkilpatrick@yext.com>
2023-06-13 12:18:05 -0400

    rp fix test

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,4 @@ import yextSSG from "@yext/pages/vite-plugin";
 export default defineConfig({
   // base: "/subdirectory/",
   plugins: [react(), yextSSG()],
-  experimental: {
-    renderBuiltUrl(filename, { hostId, hostType, type }) {
-      if (hostType === 'js') {
-        if (filename.at(0) === '/') {
-          return filename.substring(1);
-        }
-        return filename;
-      }
-      return { relative: true }
-
-      // return "hostId:" + hostId + "hostType:" + JSON.stringify(hostType) + "type:" + type + "filename:" + filename;
-    }
-  }
 });
