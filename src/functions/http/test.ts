285ef6de4c71897c2327e9d88b36afb9de084f6b
Duval Kilpatrick <mkilpatrick@yext.com>
2023-11-09 16:46:32 -0500

    add function

---
+++ src/functions/http/test.ts
@@ -0,0 +1,20 @@
+import { fetch } from "@yext/pages/util";
+
+export default async function test(request) {
+  const { pathParams, queryParams, site, body, method, headers } = request;
+
+  console.log(SECRET);
+
+  // const output = generate("Write a database schema for a hotel")
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  let json;
+  if (res.ok) {
+    json = await res.json();
+  }
+
+  return {
+    body: JSON.stringify(json) + SECRET,
+    headers: null,
+    statusCode: 200,
+  };
+}
